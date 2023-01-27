b07f45258f5002ae29c5227e5aa2cab703ee05b7
jegomezV <jegomezvalencia@gmail.com>
2023-01-27 10:54:19 -0500

    5-print_com

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0{}".format(i), end=", ")
-    else:
-        print(i, end=", ")
+    if i <= 98:
+        print(f'{i:02d}, '.format(i + 1), end="")
+    if i > 98:
+        print(i, end='\n')
