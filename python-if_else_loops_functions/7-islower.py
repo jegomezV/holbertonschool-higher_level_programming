573d8b27d115a27d81e12a90e37ce10a240ed67c
jegomezV <jegomezvalencia@gmail.com>
2023-01-27 12:20:39 -0500

    7-islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(96, 123):
-        if c == i:
+        if c == 97:
+            return islower()
+        elif c == i:
             return islower()
         else:
             "upper"
