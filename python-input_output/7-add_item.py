7336e78e590cc2b83037a3b29a18ae882d980f8b
jegomezV <jegomezvalencia@gmail.com>
2023-02-17 21:28:50 -0500

    P3

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-""" script that adds arguments to a python list and save them """
-import os.path
-from sys import argv
-""" sys allows me to receive the string to add in the file from the cl
-for accessing the filesystem os.path """
+"""Seventh"""
+
+import os
+import sys
+import json
 
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+filename = 'add_item.json'
+args = len(sys.argv)
 
-p_list = []
-if os.path.exists("add_item.json"):
-    p_list = load_from_json_file("add_item.json")
-save_to_json_file(p_list + argv[1:], "add_item.json")
+if not os.path.isfile(filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('[]')
+
+if args > 1:
+    data = load_from_json_file(filename)
+    for i in range(1, args):
+        data.append(sys.argv[i])
+    save_to_json_file(data, filename)
