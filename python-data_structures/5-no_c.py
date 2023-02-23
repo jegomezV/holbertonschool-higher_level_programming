e51dc736ad36209c9963051983ecf37cf46deb45
jegomezV <jegomezvalencia@gmail.com>
2023-02-22 20:42:01 -0500

    p1

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
-    for value in my_string
-        if (value != "c" or value != "C")
-            new_str += value
+    for char in my_string:
+        if (char != 'c' and char != 'C'):
+            new_str += char
     return new_str
