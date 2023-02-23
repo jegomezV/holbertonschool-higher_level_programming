562eebbd86dcd023e53a2f4843d91779b513cd0d
jegomezV <jegomezvalencia@gmail.com>
2023-02-23 07:48:30 -0500

    p1

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    check_m = []
+    check_multiples = []
     for i in range(len(my_list)):
-        if (my_list[i] % 2 == 0):
-            check_m.append(True)
+        if my_list[i] % 2 == 0:
+            check_multiples.append(True)
         else:
-        check_m.append(False)
-    return check_m
+            check_multiples.append(False)
+    return check_multiples
