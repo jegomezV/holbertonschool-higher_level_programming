47372a50e558016967afc18909d8fb983cf787c4
jegomezV <jegomezvalencia@gmail.com>
2023-07-21 13:10:15 -0500

    0-safe_print new_correction

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for index in range(x):
-            print("{}".format(my_list[index], end=""))
+        for index in range(0, x):
+            print("{}".format(my_list[index]), end="")
         print()
         return x
     except (TypeError, IndexError):
