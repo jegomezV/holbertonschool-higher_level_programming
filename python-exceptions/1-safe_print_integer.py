91055b19f485119d974d68312df43a1c36124866
jegomezV <jegomezvalencia@gmail.com>
2023-07-21 13:53:12 -0500

    1-safe_print_integer

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}\n".format(value))
+        return True
+    except ValueError:
+        print()
+        return False
