f7316e4ee11340debc9d39fbbc4b9e2999141da4
jegomezV <jegomezvalencia@gmail.com>
2023-07-21 18:21:54 -0500

    3-square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/python3#!/usr/bin/python3
+"""Script to create class Square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """Conditionals"""
+        self.__size = size
+        if type(size) is not int:
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """Define square area"""
+        return self.size**2
