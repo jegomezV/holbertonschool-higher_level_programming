d23b0edefbf4aad013cc996cfd1b4f9ceed8f5bf
jegomezV <jegomezvalencia@gmail.com>
2023-07-21 13:08:09 -0500

    0-safe_print correction

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for index in range(x):
-            print("{}".format(my_list[index]))
+            print("{}".format(my_list[index], end=""))
         print()
         return x
     except (TypeError, IndexError):
