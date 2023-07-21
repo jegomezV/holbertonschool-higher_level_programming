e38536b363dfe5dfbcfe94e11501b0fe9a852c69
jegomezV <jegomezvalencia@gmail.com>
2023-07-21 16:46:44 -0500

    correct - 4-list_division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     list = []
-    for index in range(len(list_length)):
+    for index in range(0, list_length):
         try:
             div = my_list_1[index] / my_list_2[index]
         except ZeroDivisionError:
