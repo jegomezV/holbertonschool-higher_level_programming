88218a74bcda264b08c9cc96233174fbd2f6f9ab
jegomezV <jegomezvalencia@gmail.com>
2023-07-21 13:55:00 -0500

    correct / 1-safe_print_integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     try:
         print("{:d}\n".format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         print()
         return False
