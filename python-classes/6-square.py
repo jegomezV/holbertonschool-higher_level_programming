a7c74f1e99bf1bc33d6287d231c1b5e47b28b937
jegomezV <jegomezvalencia@gmail.com>
2023-07-21 20:25:11 -0500

    6-square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,1 +1,72 @@
 #!/usr/bin/python3
+"""Class Square defines a Square"""
+
+
+class Square():
+    """Class initiation for square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Definition of the class Square, the value size
+        must be an integer >= 0
+        Args:
+            size (int): private attribute
+            position (tuple): private attributte
+        """
+
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Return the size of the square"""
+
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        """set the self.__size to value, the value must be a
+        integer and >= 0"""
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = value
+
+    @property
+    def position(self):
+        """Return the position of the square"""
+
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        """set the self.__size to value, the value must be a
+        integer and >= 0"""
+        if isinstance(value, tuple) is False or len(value) != 2\
+                or isinstance(value[0], int) is False\
+                or isinstance(value[1], int) is False\
+                or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self._position = value
+
+    def area(self):
+        """Return area of the square"""
+
+        return self.size**2
+
+    def my_print(self):
+        """print the square using '#'"""
+
+        if self.size == 0:
+            print()
+        else:
+            for height in range(self.position[1]):
+                print()
+            for large in range(self.size):
+                for space in range(self.position[0]):
+                    print(" ", end="")
+                for index in range(self.size):
+                    print('#', end="")
+                print()
