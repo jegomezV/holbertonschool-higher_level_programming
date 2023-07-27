627b0af8906302ba65e3fc0672dc754030878b97
jegomezV <jegomezvalencia@gmail.com>
2023-07-27 11:04:02 -0500

    Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""the class Rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Definition of the class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
+
+    @property
+    def heigh(self):
+        return self.__heigh
+
+    @heigh.setter
+    def heigh(self, value):
+        self.__heigh = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
