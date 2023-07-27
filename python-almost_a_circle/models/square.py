206b8fd9f797a5ae70c0368c90d1cdded0bce18b
jegomezV <jegomezvalencia@gmail.com>
2023-07-27 15:12:46 -0500

    Update del update con string

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,6 +7,7 @@ class Square(Rectangle):
     """Define Square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """__init__ method"""
         super().__init__(size, size, x, y, id)
         self.size = size
 
